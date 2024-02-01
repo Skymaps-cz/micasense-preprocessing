05a6085674b2379d73619ea2acc39faab3af9c4d
Michal Dolnik <dolnik.michal@gmail.com>
2024-02-01 09:20:18 -0300

    copy exif data

--- batch_sun_sensor.py
+++ batch_sun_sensor.py
@@ -8,9 +8,10 @@ import math
 import matplotlib.pyplot as plt
 import micasense.imageset as imageset
 import pandas as pd
+import subprocess
 
 
-images_path = os.path.expanduser(os.path.join('~','Downloads','medium_dataset_altum', 'MS','0000SET', '004'))
+images_path = os.path.expanduser(os.path.join('~','Downloads', '_micasense_preprocessing_data', '4347', '0002SET'))
 
 imlist = imageset.ImageSet.from_directory(images_path)
 
@@ -19,6 +20,8 @@ generateThumbnails = True
 overwrite = False
 warp_matrices = None
 ground_alt = None
+
+np.float = float
 
 
 outputPath_for_stacked = os.path.join(images_path,'..','stacks')
@@ -38,7 +41,7 @@ for i,cap in enumerate(imlist.captures):
 
     image_name_blue = cap.images[0].meta.get_item("File:FileName")
     image_name = image_name_blue[0:-6] # remove '_1.tif'
-    outputFilename = image_name+'.tif'
+    outputFilename = image_name+'.tif'  
         
     full_outputPath_for_stacked = os.path.join(outputPath_for_stacked, outputFilename)
 
@@ -47,7 +50,7 @@ for i,cap in enumerate(imlist.captures):
 
             print(cap.dls_irradiance())
 
-            irradiance = dls_coef * cap.dls_irradiance()
+            irradiance = cap.dls_irradiance()
             
             cap.create_aligned_capture(irradiance_list=irradiance, warp_matrices = warp_matrices) # convert to reflectance
             
@@ -59,68 +62,34 @@ for i,cap in enumerate(imlist.captures):
                 cap.save_capture_as_rgb(fullThumbnailPath)
     cap.clear_image_data()
 
-    # save_meta = saveMetadata(cap, outputPath_for_stacked)
 
 
 
-# images_path = os.path.expanduser(os.path.join('~','Downloads','medium_dataset_altum', 'MS','0000SET', '004'))
-# image_names = glob.glob(os.path.join(images_path,'IMG_*.tif'))
-# print(len(image_names))
-# cap = capture.Capture.from_filelist(image_names)
+def copy_exif_data_to_stack(source_dir, stack_dir):
+    source_tiffs = glob.glob(os.path.join(source_dir,'**/*_1.tif'))
 
-# print(cap)
+    stacked_tiffs = glob.glob(os.path.join(stack_dir,'*.tif'))
+    print(stacked_tiffs)
+    
+    # Ensure the source file exists
+    if not os.path.isdir(source_dir):
+        raise FileNotFoundError(f"Source file not found: {source_dir}")
 
-# # Define DLS sensor orientation vector relative to dls pose frame
-# dls_orientation_vector = np.array([0,0,-1])
-# # compute sun orientation and sun-sensor angles
-# (
-#     sun_vector_ned,    # Solar vector in North-East-Down coordinates
-#     sensor_vector_ned, # DLS vector in North-East-Down coordinates
-#     sun_sensor_angle,  # Angle between DLS vector and sun vector
-#     solar_elevation,   # Elevation of the sun above the horizon
-#     solar_azimuth,     # Azimuth (heading) of the sun
-# ) = dls.compute_sun_angle(cap.location(),
-#                       cap.dls_pose(),
-#                       cap.utc_time(),
-#                       dls_orientation_vector)
+    # Ensure the target file exists
+    if not os.path.isdir(stack_dir):
+        raise FileNotFoundError(f"Target file not found: {stack_dir}")
+    
+    for stacked_tiff in stacked_tiffs:
+        stacked_filename = stacked_tiff.split('/')[-1].split('.')[0]
+        print('stacked_filename', stacked_filename)
+        source_tiff = [tiff for tiff in source_tiffs if stacked_filename in tiff][0]
+        print('source_tiff', source_tiff)
+        
+        # Command to copy EXIF data from source to target
+        command = f"exiftool -config exiftool.config -overwrite_original -TagsFromFile {source_tiff} -all:all {stacked_tiff}"
 
-# # Since the diffuser reflects more light at shallow angles than at steep angles,
-# # we compute a correction for this
-# fresnel_correction = dls.fresnel(sun_sensor_angle)
-
-
-
-# # Now we can correct the raw DLS readings and compute the irradiance on level ground
-# dls_irradiances = []
-# center_wavelengths = []
-# for img in cap.images:
-#     dir_dif_ratio = 6.0
-#     percent_diffuse = 1.0/dir_dif_ratio
-#     # measured Irradiance / fresnelCorrection
-#     sensor_irradiance = img.spectral_irradiance / fresnel_correction
-#     untilted_direct_irr = sensor_irradiance / (percent_diffuse + np.cos(sun_sensor_angle))
-#     # compute irradiance on the ground using the solar altitude angle
-#     dls_irr = untilted_direct_irr * (percent_diffuse + np.sin(solar_elevation))
-#     dls_irradiances.append(dls_irr)
-#     center_wavelengths.append(img.center_wavelength)
+        # Execute the command
+        subprocess.run(command, shell=True, check=True)
 
 
-# plt.scatter(center_wavelengths,dls_irradiances)
-# plt.xlabel('Wavelength (nm)')
-# plt.ylabel('Irradiance ($W/m^2/nm$)')
-# plt.show()
-
-# cap.plot_undistorted_reflectance(dls_irradiances)
-
-# panel_reflectance_by_band = [0.49] * len(cap.eo_band_names()) # This panel has an average of 49% reflectance for each EO band
-
-# panel_radiances = np.array(cap.panel_radiance())
-# irr_from_panel = math.pi * panel_radiances / panel_reflectance_by_band
-# dls_correction = irr_from_panel/dls_irradiances
-# cap.plot_undistorted_reflectance(dls_irradiances*dls_correction)
-
-# plt.scatter(cap.center_wavelengths(), cap.panel_reflectance())
-# plt.title("Panel Reflectances")
-# plt.xlabel("Wavelength (nm)")
-# plt.ylabel("Reflectance")
-# plt.show()
+# copy_exif_data_to_stack(images_path, os.path.expanduser(os.path.join('~','Downloads', '_micasense_preprocessing_data', 'Pix4D_test_Average-DNG_ALtumPT_cw-padderok_230910','stacks')))
